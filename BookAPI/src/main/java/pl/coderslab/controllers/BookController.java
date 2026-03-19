0692c4da859279a13840c731d1a905877a3c4631
smiernik1 <smiernik@visa.com>
2026-03-19 21:06:01 +0100

    Dodanie funkcjonalności 'Lista wszystkich książek'

--- BookAPI/src/main/java/pl/coderslab/controllers/BookController.java
+++ BookAPI/src/main/java/pl/coderslab/controllers/BookController.java
@@ -1,12 +1,28 @@
 package pl.coderslab.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.coderslab.model.Book;
+import pl.coderslab.services.MockBookService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/books")
 public class BookController {
+
+    private final MockBookService mockBookService;
+
+    @Autowired
+    public BookController(MockBookService mockBookService) {
+        this.mockBookService = mockBookService;
+    }
+
+    @RequestMapping()
+    public List<Book> get() {
+        return mockBookService.getBooks();
+    }
 
     @RequestMapping("/helloBook")
     public Book helloBook() {
--- BookAPI/src/main/java/pl/coderslab/services/MockBookService.java
+++ BookAPI/src/main/java/pl/coderslab/services/MockBookService.java
@@ -1,18 +1,29 @@
 package pl.coderslab.services;
 
+import org.springframework.stereotype.Service;
 import pl.coderslab.interfaces.BookService;
 import pl.coderslab.model.Book;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class MockBookService implements BookService {
 
     private List<Book> list;
+    public MockBookService() {
+        list = new ArrayList<>();
+        list.add(new Book(1L, "9788324631766", "Thinking in Java","Bruce Eckel", "Helion", "programming"));
+        list.add(new Book(2L, "9788324627738", "Rusz glowa Java.", "Sierra Kathy,	Bates Bert", "Helion",
+                "programming"));
+        list.add(new Book(3L, "9780130819338", "Java 2. Podstawy","Cay Horstmann, Gary Cornell", "Helion",
+                "programming"));
+    }
 
     @Override
     public List<Book> getBooks() {
-        return List.of();
+        return list;
     }
 
     @Override
