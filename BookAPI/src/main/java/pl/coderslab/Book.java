0227b37f7f2b3eb14ea2004bc874662df1d4abd1
smiernik1 <smiernik@visa.com>
2026-03-19 19:56:42 +0100

    Utworzenie projektu

---
+++ BookAPI/src/main/java/pl/coderslab/Book.java
@@ -0,0 +1,13 @@
+package pl.coderslab;
+
+public class Book {
+    private int id;
+    private long isbn;
+    private String title;
+    private String author;
+    private String publisher;
+    private String type;
+
+    public Book() {
+    }
+}
