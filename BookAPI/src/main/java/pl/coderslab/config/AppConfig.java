940fc65b513578864546f8f23eb46a19065ecb8b
smiernik1 <smiernik@visa.com>
2026-03-19 20:24:06 +0100

    Podstawowa wersja - działąjąca

--- BookAPI/src/main/java/pl/coderslab/config/AppConfig.java
+++ BookAPI/src/main/java/pl/coderslab/config/AppConfig.java
@@ -2,6 +2,7 @@ package pl.coderslab.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -10,6 +11,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @ComponentScan(basePackages = "pl.coderslab")
 public class AppConfig implements WebMvcConfigurer {
 
-
+    @Override
+    public void configureDefaultServletHandling(
+            DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
 }
 
--- BookAPI/src/main/java/pl/coderslab/controllers/BookController.java
+++ BookAPI/src/main/java/pl/coderslab/controllers/BookController.java
@@ -10,7 +10,8 @@ public class BookController {
 
     @RequestMapping("/helloBook")
     public Book helloBook() {
-        return new Book(1L, "9788324631766", "Thinking in Java", "Bruce Eckel", "Helion", "programming");
+        return new Book(1L, "9788324631766", "Thinking in Java",
+                "Bruce Eckel", "Helion", "programming");
     }
 }
 
